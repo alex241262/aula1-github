f37419fcc36970a95ae5befd20195e827ecf634a
alexandre lopes <alexandreferreira168@gmail.com>
2020-02-27 09:51:44 -0400

    Alterado program

--- src/applocation/Program.java
+++ src/applocation/Program.java
@@ -9,14 +9,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Ola voce entrou no Guithub");
-		Order order = new Order(1080,new Date(),OrderStatus.PENDING_PAYMENT);
+		System.out.println("Ola Bom Dia");
+//teste de orden status
+		/*Order order = new Order(1080,new Date(),OrderStatus.PENDING_PAYMENT);
 		
 		System.out.println(order);
 		OrderStatus os1 = OrderStatus.DELIVERED;
 		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
 		
 		System.out.println(os1);
-		System.out.println(os2);
+		System.out.println(os2);*/
 
 	}
 
