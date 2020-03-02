d6f77df78923d4bc0336ae99e0f4c346afdcf529
alexandre lopes <alexandreferreira168@gmail.com>
2020-03-02 14:00:00 -0400

    Terminado progrma

--- src/applocation/Program.java
+++ src/applocation/Program.java
@@ -1,9 +1,13 @@
 package applocation;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
-import entities.Order;
-import entities.enums.OrderStatus;
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
 
 public class Program {
 
@@ -19,7 +23,43 @@ public class Program {
 		
 		System.out.println(os1);
 		System.out.println(os2);*/
-
+		Locale.setDefault(Locale.US);
+		Scanner ler = new Scanner(System.in);
+		SimpleDateFormat sdf =new  SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Enter department´s name: ");
+		String departmentName= ler.next();
+		System.out.println("Enter worker data: ");
+		System.out.println("Name: ");
+		String workerName= ler.next();
+		System.out.println("Level: ");
+		String workerLevel= ler.next();
+		System.out.print("Base Salary: ");
+		double baseSalary = ler.nextDouble();
+		Worker worker = new Worker(workerName,WorkerLevel.valueOf(workerLevel),baseSalary,new Department(departmentName));
+		
+		System.out.print("How many contracts to this worker? ");
+		int n = ler.nextInt();
+		for(int i=1;i<=n;i++){
+			System.out.println("Enter contact n"+ i +" data:");
+			System.out.println("Date(DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(ler.next());
+			System.out.print("Valur per hour: ");
+			double valuePerHour = ler.nextDouble();
+			System.out.println("Duration (hours): ");
+			int hours = ler.nextInt();
+			HourContract contract = new HourContract(contractDate,valuePerHour,hours);
+			worker.addContract(contract);
+		}
+		System.out.println("Enter month and year to calculete incone (MM/YYYY): ");
+		String monthAndYear = ler.next();
+		int month = Integer.parseInt(monthAndYear.substring(0,2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: "+worker.getName());
+		System.out.println("Derpatment: "+worker.getDepartment().getName());
+		System.out.println("Income for "+ monthAndYear +": "+String.format("%.2f", worker.income(year,month)));
+		
+		ler.close();
 	}
 
 }
